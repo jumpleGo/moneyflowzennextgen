f5493e31beee91f1bc34fee19f5bc7de2b2ea162
Emil Latypov <rrotatew@gmail.com>
2024-12-14 10:39:59 +0800

    improvements

--- components/Exchanger/NotificationBlock.vue
+++ components/Exchanger/NotificationBlock.vue
@@ -15,7 +15,7 @@ const text = computed(() => data[props.notifyType])
 
 const data = {
   'bugs': 'Сервис новый! Могут быть баги. Это нормально.',
-  'time': 'Активное время работы: 5:00 - 21:00 МСК. В иное время уточняйте статус онлайна у <a href="https://t.me/mfz_owner" target="_blank">@mfz_owner</a>'
+  'time': 'Временно, активное время работы: 4:00 - 19:00 МСК. В иное время ваш платеж будет обработан в следующий рабочий день.'
 }
 </script>
 <style lang="scss" scoped>
