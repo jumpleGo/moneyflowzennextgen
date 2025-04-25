121317243407afd3acfb77c2db05e6247697a71c
Emil Latypov <rrotatew@gmail.com>
2025-04-26 00:47:24 +0300

    scheme

--- server/api/sitemap/sitemap.ts
+++ server/api/sitemap/sitemap.ts
@@ -1,21 +1,29 @@
 import { useServerDatabase } from '~/server/utils/firebase'
 import { child, get, query } from 'firebase/database'
-import { defineEventHandler } from 'h3'
+import { defineEventHandler, send } from 'h3'
 import { IBlogItem } from '~/types/pages/blog'
 
-export default defineEventHandler(async () => {
+export default defineEventHandler(async (event) => {
   const { databaseRef } = useServerDatabase()
-
   const request = query(child(databaseRef, 'blog'))
   const snapshot = await get(request)
   const value: Record<string, IBlogItem> = snapshot.val()
 
   if (!value) {
-    return []
+    return send(event, '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"></urlset>', 'application/xml')
   }
 
-  return Object.values(value).map((item) => ({
-    loc: `/blog/${item.key}`, // <-- должно быть `loc`, а не `url`
-    lastmod: new Date(item.updatedAt * 1000).toISOString(), // <-- Дата в формате ISO строки
-  }))
+  const urls = Object.values(value)
+    .map(item => `
+    <url>
+      <loc>https://moneyflowzen.ru/blog/${item.key}</loc>
+      <lastmod>${new Date(item.updatedAt * 1000).toISOString()}</lastmod>
+    </url>`).join('')
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`
+
+  return send(event, sitemap, 'application/xml')
 })
